42f24470c74a0e649594a4083dc715854587d733
Ravjot <ravjot28@gmail.com>
2014-01-03 23:49:08 +0530

    Integrated Remote project with Maven

---
+++ src/main/java/com/fameden/bean/user/FamedenUserKeysBean.java
@@ -0,0 +1,39 @@
+package com.fameden.bean.user;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "FAMEDEN_USER_KEYS")
+public class FamedenUserKeysBean implements Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue( strategy = GenerationType.AUTO)
+	private int famdenInternalUserId;
+	private String password;
+
+	public int getFamdenInternalUserId() {
+		return famdenInternalUserId;
+	}
+
+	public void setFamdenInternalUserId(int famdenInternalUserId) {
+		this.famdenInternalUserId = famdenInternalUserId;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+}
